d88d4b8453b03513935781beee962a751f76894b
pauliescanlon <pauliescanlon@gmail.com>
2020-07-13 17:39:23 +0100

    update style-guide

--- gatsby-theme-apple-store/src/gatsby-plugin-theme-ui/index.js
+++ gatsby-theme-apple-store/src/gatsby-plugin-theme-ui/index.js
@@ -1,20 +1,21 @@
+import { lighten } from '@theme-ui/color'
 import codeTheme from '@theme-ui/prism/presets/oceanic-next.json'
 
 export default {
   colors: {
     text: '#24292E',
-    background: '#FFFFFF',
-    primary: '#34D058',
-    secondary: '#0366D6',
-    muted: '#CCCCCC',
-    highlight: '#BBFFCD',
-    gray: '#808080',
-    accent: '#cee5ff',
+    background: '#ffffff',
+    primary: '#eb3837',
+    secondary: '#2b3252',
+    muted: '#f7f7f7',
+    highlight: '#fdb5b5',
+    gray: '#a0a0a0',
+    accent: '#fad744',
     darken: '#444444',
   },
   fonts: {
-    heading: 'Ruda',
-    body: 'Roboto Slab',
+    body: 'Ruda',
+    heading: 'Roboto Slab',
     monospace: 'monospace',
   },
   fontWeights: {
@@ -23,28 +24,30 @@ export default {
     bold: 700,
   },
   lineHeights: {
-    body: 1.75,
-    heading: 1.125,
+    body: 2.2,
+    heading: 1.1,
   },
   fontSizes: [12, 16, 18, 28, 32, 64],
-  space: [2, 4, 8, 16, 32, 48, 64],
-
+  space: [0, 4, 8, 16, 32, 48, 64],
   sizes: {
-    container: 640,
+    container: 940,
   },
   layout: {
     container: {
       p: [2, 4],
     },
   },
-
   text: {
     default: {
       fontFamily: 'body',
     },
+    heading: {
+      fontFamily: 'heading',
+      lineHeight: 'heading',
+      m: 0,
+    },
   },
   breakpoints: [540, 720, 960, 1140],
-
   styles: {
     root: {
       fontFamily: 'body',
@@ -52,23 +55,142 @@ export default {
       fontSize: 0,
       lineHeight: 'body',
     },
-    h1: { fontFamily: 'heading', fontSize: 5 },
-    h2: { fontFamily: 'heading', fontSize: 4 },
-    h3: { fontFamily: 'heading', fontSize: 3 },
-    h4: { fontFamily: 'heading', fontSize: 2 },
-    h5: { fontFamily: 'heading', fontSize: 1 },
-    h6: { fontFamily: 'heading', fontSize: 0 },
+    h1: { variant: 'text.heading', fontSize: 5 },
+    h2: { variant: 'text.heading', fontSize: 4 },
+    h3: { variant: 'text.heading', fontSize: 3 },
+    h4: { variant: 'text.heading', fontSize: 2 },
+    h5: { variant: 'text.heading', fontSize: 1 },
+    h6: { variant: 'text.heading', fontSize: 0 },
+    a: {
+      color: 'secondary',
+      fontFamily: 'body',
+      ':focus': {
+        outlineColor: 'accent',
+      },
+    },
     p: {
       fontFamily: 'body',
       fontSize: 0,
       code: {
         px: 0,
+        whiteSpace: 'pre-wrap',
         ...codeTheme,
       },
     },
     pre: {
       p: 3,
+      whiteSpace: 'pre-wrap',
       ...codeTheme,
+    },
+    hr: {
+      my: 4,
+      borderStyle: 'solid',
+      borderColor: 'muted',
+    },
+    donut: {
+      secondary: {
+        stroke: 'secondary',
+      },
+    },
+    progress: {
+      secondary: {
+        color: 'secondary',
+      },
+    },
+    spinner: {
+      secondary: {
+        color: 'secondary',
+      },
+    },
+  },
+  links: {
+    nav: {
+      fontSize: 1,
+      fontWeight: 'bold',
+      ':focus': {
+        outlineColor: 'highlight',
+      },
+    },
+  },
+  alerts: {
+    secondary: {
+      backgroundColor: 'secondary',
+    },
+  },
+  badges: {
+    default: {
+      px: 2,
+    },
+    primary: {
+      variant: 'badges.default',
+    },
+    secondary: {
+      variant: 'badges.default',
+      backgroundColor: 'secondary',
+    },
+  },
+  messages: {
+    primary: {
+      borderLeftColor: 'primary',
+      backgroundColor: lighten('primary', 0.3),
+    },
+    secondary: {
+      borderLeftColor: 'secondary',
+      backgroundColor: lighten('secondary', 0.68),
+    },
+  },
+  buttons: {
+    default: {
+      cursor: 'pointer',
+      display: 'inline-flex',
+      placeItems: 'center',
+      textTransform: 'uppercase',
+      ':focus': {
+        outlineColor: 'highlight',
+      },
+    },
+    primary: {
+      variant: 'buttons.default',
+    },
+    secondary: {
+      variant: 'buttons.default',
+      backgroundColor: 'secondary',
+    },
+    icon: {
+      variant: 'buttons.default',
+    },
+    menu: {
+      variant: 'buttons.default',
+    },
+    close: {
+      variant: 'buttons.default',
+    },
+  },
+  forms: {
+    input: {
+      ':focus': {
+        outlineColor: 'highlight',
+      },
+    },
+    select: {
+      ':focus': {
+        outlineColor: 'highlight',
+      },
+    },
+    textarea: {
+      ':focus': {
+        outlineColor: 'highlight',
+      },
+    },
+    label: {
+      cursor: 'pointer',
+    },
+    slider: {
+      color: 'gray',
+      backgroundColor: 'muted',
+      ':focus': {
+        color: 'primary',
+      },
     },
   },
 }
